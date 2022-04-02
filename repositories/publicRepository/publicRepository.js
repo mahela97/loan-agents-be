26f35b2e0c942524a38d250f7eeff85c637b8a7e
Mahela Pradeep <61684744+mahela97@users.noreply.github.com>
2022-04-02 17:08:41 +0530

    Update publicRepository.js

--- repositories/publicRepository/publicRepository.js
+++ repositories/publicRepository/publicRepository.js
@@ -2,6 +2,7 @@ const knex = require("../../db/db-config");
 const {SITE_META_DATA_TABLE} = require("../../constants/const");
 module.exports = {
     getSiteMetaData:async(req,res)=>{
-        res.status(200).send( await knex(SITE_META_DATA_TABLE).select("*"));
+        const result = await knex(SITE_META_DATA_TABLE).select("*");
+        res.status(200).send(result[0] );
     }
 }
