57180a3ca6425cc6c7ab4c5239f5b386227c3a27
mahela97 <m.pradeepd@gmail.com>
2022-04-09 15:52:23 +0530

    fix

--- controllers/userControllers/agentController.js
+++ controllers/userControllers/agentController.js
@@ -21,8 +21,13 @@ module.exports = {
             res.status(201).send(agentDetails)
 
         }catch(error){
-            console.log(error)
             res.status(401).send(error)
         }
+    },
+
+    editAgentDetails:async (req,res)=>{
+        const schema = Joi.object({
+            
+        })
     }
 }
