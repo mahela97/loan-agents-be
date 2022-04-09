1f11ac84c0b2cf7c2bdc7ff0d2cbc1ae308a4922
mahela97 <m.pradeepd@gmail.com>
2022-04-09 18:44:10 +0530

    edit user details

--- controllers/userControllers/agentController.js
+++ controllers/userControllers/agentController.js
@@ -1,18 +1,19 @@
 const Joi = require("joi");
-const {getAgentDetails} = require("../../services/agentService");
+const {getAgentDetails, editAgentBasicDetails} = require("../../services/agentService");
+const handleFirebase = require("../../utils/firebaseErrorhandler");
 module.exports = {
-    getAgent:async (req,res)=>{
+    getAgent: async (req, res) => {
         const schema = Joi.object({
-            uid:Joi.string().required()
+            uid: Joi.string().required()
         })
 
         const validate = schema.validate(req.query)
-        if(validate.error){
+        if (validate.error) {
             res.status(400).send(validate.error.message)
             return
         }
         const {uid} = validate.value
-        try{
+        try {
             const agentDetails = await getAgentDetails(uid);
             if (!agentDetails) {
                 res.status(404).send("User not found");
@@ -20,15 +21,43 @@ module.exports = {
             }
             res.status(201).send(agentDetails)
 
-        }catch(error){
+        } catch (error) {
             console.log(error)
             res.status(401).send(error)
         }
     },
 
-    editAgentDetails:async (req,res)=>{
+    editAgentDetails: async (req, res) => {
         const schema = Joi.object({
-            
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required(),
+            statement: Joi.string().required(),
+            location: Joi.string().required(),
         })
+
+        const pathSchema = Joi.object({
+            uid: Joi.string().required()
+        });
+        const validate = schema.validate(req.body, {abortEarly: false});
+        if (validate.error) {
+            res.status(400).send({message: validate.error.details});
+            return;
+        }
+
+        const pathValidate = pathSchema.validate(req.params, {abortEarly: false})
+        if (pathValidate.error) {
+            res.status(400).send({message: pathValidate.error.details})
+            return;
+        }
+        const body = validate.value;
+        const {uid} = pathValidate.value
+        try {
+            await editAgentBasicDetails(uid, body);
+            res.status(200).send({success: 1});
+        } catch (error) {
+            console.log(error)
+            if (error.message) res.status(400).send(error.message);
+            else if (error) res.status(400).send(error);
+        }
     }
 }
--- repositories/userRepositories/agentRepository.js
+++ repositories/userRepositories/agentRepository.js
@@ -1,9 +1,16 @@
 const knex = require("../../db/db-config");
 const {AGENT_DETAIL_TABLE, COMMON} = require("../../constants/const");
 module.exports = {
-    getAgentDetailsByUid:async (uid)=>{
-        const agentDetails = await knex(AGENT_DETAIL_TABLE.NAME).select(COMMON.SELECT_ALL).
-        where(AGENT_DETAIL_TABLE.USER_ID,uid);
+    getAgentDetailsByUid: async (uid) => {
+        const agentDetails = await knex(AGENT_DETAIL_TABLE.NAME).select(COMMON.SELECT_ALL).where(AGENT_DETAIL_TABLE.USER_ID, uid);
         return agentDetails
+    },
+
+    updateAgentDetails: async (uid, details, transaction) => {
+        if (transaction) {
+            await knex(AGENT_DETAIL_TABLE.NAME).transacting(transaction).update(details).where(AGENT_DETAIL_TABLE.USER_ID, uid)
+        } else {
+            await knex(AGENT_DETAIL_TABLE.NAME).update(details).where(AGENT_DETAIL_TABLE.USER_ID, uid)
+        }
     }
 }
